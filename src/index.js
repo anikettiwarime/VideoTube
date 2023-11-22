17761e52d7c8a9d5d1005a046084b757acb0bfca
Aniket Tiwari <anikettiwari1570@gmail.com>
2023-11-22 22:22:47 +0530

    All Setup completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import connectDB from './db/connectDB.js';
+import { app } from './app.js';
 
-connectDB();
+
+connectDB()
+    .then(
+        app.listen(process.env.PORT, () => {
+            console.log(`🚀 Server listening on port ${process.env.PORT}`)
+        })
+    )
+    .catch(err => console.log(err));
+
